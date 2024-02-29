3be79b23098cd76eb7faab70be1efb81a2d5ad94
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-02-29 17:04:30 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+        return response.content
+        
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+    
+    
+resource = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(resource.get_response_body())
+# print(resource.load_json())
